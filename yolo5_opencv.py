a9b6dae57e95227fffc70cf14a2d730442798b3b
Ussama Naal <ussama.naal@ouster.io>
2024-04-02 14:41:51 -0700

    Utilize the updated ScanSource interface

--- yolo5_opencv.py
+++ yolo5_opencv.py
@@ -1,55 +1,15 @@
 import time
+import math
 import cv2
 import numpy as np
 import torch
 from PIL import Image
-import itertools
 
-from ouster import client
-from ouster.client import ChanField, LidarScan
-from ouster.pcap import Pcap
-from ouster.sdk.util import resolve_metadata
-from ouster.client._utils import AutoExposure
+from ouster.sdk import client
+from ouster.sdk.client import ChanField
+from ouster.sdk.client._utils import AutoExposure
+from ouster.sdk import open_source
 
-class ScanCapture:
-    def __init__(self, source, loop=True) -> None:
-        self._loop = loop
-        self._open_pcap(source)
-    @property
-    def sensor_info(self):
-        return self._info
-    @property
-    def scans_loop(self) -> LidarScan:
-        return self._scans_loop
-    def _open_pcap(self, file_path):
-        meta_file = resolve_metadata(file_path)
-        with open(meta_file, "r") as f:
-            self._info = client.SensorInfo(f.read())
-        if self._info:
-            self._scans = client.Scans(Pcap(file_path, self._info), complete=True)
-        else:
-            print("error opening the stream")
-        # Use this only on short clips
-        self._scans_loop = itertools.cycle(self._scans) if self._loop else None
-    def _read(self):
-        scans = self._scans_loop if self._loop else self._scans
-        for s in scans:
-            yield s
-    def read(self) -> LidarScan:
-        try:
-            return True, next(self._read())
-        except Exception as e:
-            print(e)
-            return False, None
-    def release(self):
-        self._scans.close()
-
-# Open the pcap file
-pcap_file_path = "./Ouster-YOLOv5-sample.pcap"
-scan_capture = ScanCapture(pcap_file_path, loop=False)
-
-# Load YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 def model_infer(model, frame):
     image = Image.fromarray(frame * 255)
@@ -58,13 +18,28 @@ def model_infer(model, frame):
     return results.pred[0][class_indices == 0, :4]
 
 
-def draw_results(image, objects):
+def draw_results(image, objects, sensor_info, scan, xyzlut):
+    """draw detected persons with distance"""
+    xyz_destaggered = client.destagger(sensor_info, xyzlut(scan))
+    range_val = scan.field(ChanField.RANGE).astype(np.float32)
+
     for box in objects:
         x1, y1, x2, y2 = map(int, box)
+        range_roi = range_val[y1:y2, x1:x2]
+        # only consider points /w valid returns
+        valid_returns_idx = np.where(range_roi > 0)
+        poi_roi = np.unravel_index(range_roi[valid_returns_idx].argmin(),
+                                   range_roi.shape)  # (y,x) in roi
+        poi_x = poi_roi[1] + x1
+        poi_y = poi_roi[0] + y1
+        poi = (poi_y, poi_x)  # (y,x) in global
+        xyz = xyz_destaggered[poi]
+        dist = math.sqrt(xyz[0] ** 2 + xyz[1] ** 2 + xyz[2] ** 2)
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 1, 0), 1)
-        cv2.putText(image,'person', (x1, y1),
+        cv2.putText(image, F"person {round(dist,2)} m", (x1, y1),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     0.4, (0, 1, 0), 1, 2)
+
 
 # create a post processor for every channel to perform auto leveling
 post_processors = {
@@ -74,70 +49,75 @@ post_processors = {
     ChanField.NEAR_IR: AutoExposure()
 }
 
+
 def get_frame_from_scan(scan, channel):
     image = scan.field(channel).astype(np.float32)
-    image = client.destagger(scan_capture.sensor_info, image)
+    image = client.destagger(scan_source.metadata, image)
     post_processors[channel](image)
     return image
 
-# extract the scan width x heigh x fps information from the json file
-def get_scan_size_and_fps(metadata_path):
-    with open(metadata_path, 'r') as file:
-        import json
-        data = json.load(file)
-        lidar_mode = data["lidar_mode"].split('x')
-        w = int(lidar_mode[0])
-        h = len(data["beam_altitude_angles"])
-        fps = int(lidar_mode[1])
-        return w, h, fps
 
-scan_width, scan_height, scan_fps = get_scan_size_and_fps('./Ouster-YOLOv5-sample.json')
+def get_scan_size_and_fps(sensor_info: client.SensorInfo):
+    """extract the scan width x heigh x fps information from the json file"""
+    w = sensor_info.mode.cols
+    h = len(sensor_info.beam_altitude_angles)
+    fps = sensor_info.mode.frequency
+    return w, h, fps
+
+
+# Load YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Open the pcap file
+pcap_file_path = "./Ouster-YOLOv5-sample.pcap"
+
+scan_source = open_source(pcap_file_path, sensor_idx=0, cycle=True)
+scan_width, scan_height, scan_fps = get_scan_size_and_fps(scan_source.metadata)
 
 video_fps = scan_fps    # apply the same fps to the video size
 video_size = (scan_width, scan_height * 4)  # We multiply the video output by 4
-                                            # since we are going to stack the 4
-                                            # frames on top of each others vertically
+# since we are going to stack the 4
+# frames on top of each others vertically
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, video_fps, video_size)
 
+xyzlut = client.XYZLut(scan_source.metadata)  # construct the cartesian lookup table
+
 def run():
 
-    ret, scan = scan_capture.read()
-
-    if not ret:
-        return True
-
-    fields = [ChanField.RANGE, ChanField.SIGNAL, ChanField.REFLECTIVITY, ChanField.NEAR_IR]
+    fields = [ChanField.RANGE, ChanField.SIGNAL,
+              ChanField.REFLECTIVITY, ChanField.NEAR_IR]
     images = [None] * len(fields)
 
-    for i in range(len(fields)):
-        images[i] = get_frame_from_scan(scan, fields[i])
-        results = model_infer(model, images[i])
-        # convert to a colored image
-        images[i] = cv2.cvtColor(images[i], cv2.COLOR_GRAY2BGR)
-        draw_results(images[i], results)
+    for scan in scan_source:
+        # process the scan
+        start = time.time()
+        for i in range(len(fields)):
+            images[i] = get_frame_from_scan(scan, fields[i])
+            results = model_infer(model, images[i])
+            # convert to a colored image
+            images[i] = cv2.cvtColor(images[i], cv2.COLOR_GRAY2BGR)
+            draw_results(images[i], results,
+                         scan_source.metadata, scan, xyzlut)
+        end = time.time()
+        sleep_period = 1.0 / scan_fps - (end - start)
+        if sleep_period > 0:
+            time.sleep(sleep_period)
 
-    time.sleep(1.0 / scan_fps)
+        stacked_images = cv2.vconcat(images)
+        converted_image = cv2.convertScaleAbs(stacked_images * 255)
+        cv2.imshow('Lidar Detection', stacked_images)
+        out.write(converted_image)
 
-    stacked_images = cv2.vconcat(images)
-    converted_image = cv2.convertScaleAbs(stacked_images * 255)
-    cv2.imshow('Lidar Detection', stacked_images)
-    out.write(converted_image)
+        key = cv2.waitKey(1) & 0xFF
+        if key != 0xFF:
+            # Press 'ESC' to exit the loop
+            if key == 27:
+                return
 
-    key = cv2.waitKey(1) & 0xFF
-    if key != 0xFF:
-        # Press 'ESC' to exit the loop
-        if key == 27:
-            return True
 
-    return False
-
-done = False
-
-while not done:
-    done = run()
-
-scan_capture.release()
+run()
+scan_source.close()
 out.release()
 cv2.destroyAllWindows()
--- yolo5_simpleviz.py
+++ yolo5_simpleviz.py
@@ -5,59 +5,20 @@ import torch
 from PIL import Image
 import itertools
 
-from ouster import client
-from ouster.client import ChanField, LidarScan
-from ouster.pcap import Pcap
-from ouster.sdk.util import resolve_metadata
-from ouster.client._utils import AutoExposure
+from ouster.sdk import client
+from ouster.sdk.client import ChanField, LidarScan
+from ouster.sdk.client._utils import AutoExposure
+from ouster.sdk import open_source
 
-from ouster.sdk.simple_viz import SimpleViz
+from ouster.sdk.viz import SimpleViz
 
-class ScanCapture:
-    def __init__(self, source, loop=True) -> None:
-        self._loop = loop
-        self._open_pcap(source)
-    @property
-    def sensor_info(self):
-        return self._info
-    @property
-    def scans_loop(self) -> LidarScan:
-        return self._scans_loop
-    def _open_pcap(self, file_path):
-        meta_file = resolve_metadata(file_path)
-        with open(meta_file, "r") as f:
-            self._info = client.SensorInfo(f.read())
-        if self._info:
-            self._scans = client.Scans(Pcap(file_path, self._info), complete=True)
-        else:
-            print("error opening the stream")
-        # Use this only on short clips
-        self._scans_loop = itertools.cycle(self._scans) if self._loop else None
-    def _read(self):
-        scans = self._scans_loop if self._loop else self._scans
-        for s in scans:
-            yield s
-    def read(self) -> LidarScan:
-        try:
-            return True, next(self._read())
-        except Exception as e:
-            print(e)
-            return False, None
-    def release(self):
-        self._scans.close()
-
-# Open the pcap file
-pcap_file_path = "./Ouster-YOLOv5-sample.pcap"
-scan_capture = ScanCapture(pcap_file_path, loop=True)
-
-# Load YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 def model_infer(model, frame):
     image = Image.fromarray(frame * 255)
     results = model(image)
     class_indices = results.pred[0][:, -1].int()
     return results.pred[0][class_indices == 0, :4]
+
 
 def draw_results(image, objects):
     for box in objects:
@@ -73,25 +34,12 @@ post_processors = {
     ChanField.NEAR_IR: AutoExposure()
 }
 
+
 def get_frame_from_scan(scan, channel):
     image = scan.field(channel).astype(np.float32)
-    image = client.destagger(scan_capture.sensor_info, image)
+    image = client.destagger(scan_source.metadata, image)
     post_processors[channel](image)
     return image
-
-
-# extract the scan width x heigh x fps information from the json file
-def get_scan_size_and_fps(metadata_path):
-    with open(metadata_path, 'r') as file:
-        import json
-        data = json.load(file)
-        lidar_mode = data["lidar_mode"].split('x')
-        w = int(lidar_mode[0])
-        h = len(data["beam_altitude_angles"])
-        fps = int(lidar_mode[1])
-        return w, h, fps
-    
-scan_width, scan_height, scan_fps = get_scan_size_and_fps('./Ouster-YOLOv5-sample.json')
 
 
 def process_scan(scan: LidarScan) -> LidarScan:
@@ -99,18 +47,26 @@ def process_scan(scan: LidarScan) -> LidarScan:
 
     fields = [ChanField.SIGNAL, ChanField.REFLECTIVITY, ChanField.NEAR_IR]
 
-    for  f in fields:
+    for f in fields:
         frame = get_frame_from_scan(scan, f)
         results = model_infer(model, frame)
         draw_results(frame, results)
         # Stagger the frame again before storing the results again
-        scan.field(f)[:] = client.destagger(scan_capture.sensor_info, frame * 255, inverse=True)
+        scan.field(f)[:] = client.destagger(scan_source.metadata,
+                                            frame * 255, inverse=True)
 
     time.sleep(0.033)
 
     return scan
 
-scan_iterator = map(process_scan, scan_capture.scans_loop)
-SimpleViz(scan_capture.sensor_info).run(scan_iterator)
 
-scan_capture.release()
+# Open the pcap file
+pcap_file_path = "./Ouster-YOLOv5-sample.pcap"
+scan_source = open_source(pcap_file_path, sensor_idx=0, cycle=True)
+
+# Load YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+scan_iterator = map(process_scan, scan_source)
+SimpleViz(scan_source.metadata).run(scan_iterator)
+scan_source.close()
